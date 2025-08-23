67f70022fb854e3433491db3c82b0923cde88299
Ehab Amer <ehab.amer@gmail.com>
2025-08-23 16:38:55 +0200

    Adding lesson 5 starter

---
+++ 05-MetricKit/Starter/MetricsKit/MetricPayloadSender.swift
@@ -0,0 +1,115 @@
+/// Copyright (c) 2025 Kodeco LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import MetricKit
+import OpenTelemetryApi
+
+typealias SimpleHistogramBuckets = [(value: Double, count: Int)]
+
+class MetricPayloadSender {
+  let attributes: [String: AttributeValue]
+  
+  init(attributes: [String: AttributeValue]) {
+    self.attributes = attributes
+  }
+  
+  class func simpleHistogram<UnitType>(_ histogram: MXHistogram<UnitType>) -> SimpleHistogramBuckets where UnitType: Unit {
+    let buckets = histogram.bucketEnumerator.compactMap { $0 as? MXHistogramBucket }
+    
+    var simpleBuckets: SimpleHistogramBuckets {
+      buckets.map { bucket in
+        let value = (bucket.bucketStart.value + bucket.bucketEnd.value) / 2
+        return (value, bucket.bucketCount)
+      }
+    }
+    
+    return simpleBuckets
+  }
+  
+  func sendMetric(name: String, measurement: Int?) {
+    guard let measurement else { return }
+    
+    sendMetric(name: name, measurement: Double(measurement))
+  }
+  
+  func sendMetric(name: String, measurement: Double?) {
+    guard let measurement else { return }
+    
+    OTelLogs.sendEvent(
+      scope: "MetricsKit",
+      eventName: name,
+      data: ["value" : AttributeValue.double(measurement)],
+      message: "")
+  }
+  
+  func sendMetric<T: Unit>(name: String, measurement: Measurement<T>?) {
+    guard let measurement else { return }
+    
+    OTelLogs.sendEvent(
+      scope: "MetricsKit",
+      eventName: name,
+      data: ["value" : AttributeValue.double(measurement.value)],
+      message: "")
+  }
+  
+  func sendMetric(metric: ValueMetric?) {
+      guard let metric else { return }
+      
+      var metricAttributes = self.attributes
+      for (key, value) in metric.toDictionary() {
+          metricAttributes[key] = .string(value)
+      }
+      
+      OTelLogs.sendEvent(scope: "MetricsKit", eventName: metric.name, data: metricAttributes)
+  }
+  
+  func sendHistogram<T>(
+    name: String,
+    values: MXHistogram<T>?,
+    histogramInfo: HistogrammedMetric?
+  ) where T: Unit {
+    guard let values else { return }
+    let simpleBuckets = MetricPayloadSender.simpleHistogram(values)
+    
+    let tupleValues = simpleBuckets.map { bucket in
+      (measure: bucket.value, count: bucket.count)
+    }
+    
+    OTelMetrics.sendHistogram(
+      metricsGroup: "MetricsKit",
+      name: name,
+      values: tupleValues,
+      unit: histogramInfo?.unit ?? "",
+      boundaries: histogramInfo?.boundaries ?? [],
+      attributes: attributes)
+  }
+}
---
+++ 05-MetricKit/Starter/Observability/OTelSpans.swift
@@ -0,0 +1,224 @@
+/// Copyright (c) 2025 Kodeco LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Foundation
+
+import OpenTelemetryApi
+import OpenTelemetrySdk
+import OpenTelemetryProtocolExporterCommon
+import OpenTelemetryProtocolExporterHttp
+import ResourceExtension
+
+//typealias OpenTelemetry = OpenTelemetryConcurrency.OpenTelemetry
+
+public class OTelSpans {
+  private static var shared = OTelSpans()
+  
+  let sampleKey = "sampleKey"
+  let sampleValue = "sampleValue"
+  
+  var grafanaHttpExporter: OtlpHttpTraceExporter!
+  
+  private init() {
+    
+    let grafanaEndpoint = URL(string: "https://otlp-gateway-prod-eu-west-2.grafana.net/otlp/v1/traces")!
+    
+    let grafanaHeaders = OtlpConfiguration(headers: [("Authorization", "Basic \(grafanaToken)")], exportAsJson: true)
+    
+    grafanaHttpExporter = OtlpHttpTraceExporter(endpoint: grafanaEndpoint,
+                                                config: grafanaHeaders)
+    
+    let spanProcessor = SimpleSpanProcessor(spanExporter: grafanaHttpExporter)
+    
+    OpenTelemetry.registerTracerProvider(tracerProvider:
+                                          TracerProviderBuilder()
+      .with(resource: DefaultResources().get())
+      .add(spanProcessor: spanProcessor)
+      .build()
+    )
+  }
+  
+  internal func tracer(
+    scopeName: String
+  ) -> any Tracer {
+    let instrumentationScopeVersion = "semver:0.1.0"
+    
+    let tracer = OpenTelemetry.instance.tracerProvider.get(
+      instrumentationName: scopeName,
+      instrumentationVersion: instrumentationScopeVersion)
+    return tracer
+  }
+  
+  public func createSpan(
+    scopeName: String,
+    name: String,
+    attributes: [String: AttributeValue] = [:],
+    spanKind: SpanKind = .client,
+    spanStartTime: Date? = nil,
+    parentSpan: (any Span)? = nil,
+  ) -> (any Span) {
+    var spanBuilder = tracer(scopeName: scopeName).spanBuilder(spanName: name).setSpanKind(spanKind: spanKind)
+    if let parentSpan {
+      spanBuilder = spanBuilder.setParent(parentSpan)
+    }
+    
+    if let spanStartTime {
+      spanBuilder = spanBuilder.setStartTime(time: spanStartTime)
+    }
+    
+    let span = spanBuilder.startSpan()
+    
+    span.setAttributes(attributes)
+    
+    return span
+  }
+  
+  public class func createSpan(
+    scopeName: String,
+    name: String,
+    attributes: [String: AttributeValue] = [:],
+    spanKind: SpanKind = .client,
+    spanStartTime: Date? = nil,
+    parentSpan: (any Span)? = nil,
+  ) -> (any Span) {
+    shared.createSpan(scopeName: scopeName,
+                      name: name,
+                      attributes: attributes,
+                      spanKind: spanKind,
+                      spanStartTime: spanStartTime,
+                      parentSpan: parentSpan)
+  }
+}
+
+public extension Span {
+  func end(_ status: Status) {
+    self.status = status
+    end()
+  }
+}
+
+//public func withSpan<T>(_ operationName: String,
+//                        scopeName: String = "OTelSpans",
+//                        ofKind kind: SpanKind = .internal,
+//                        function: String = #function,
+//                        file: String = #fileID,
+//                        line: UInt = #line,
+//                        _ operation: (any SpanBase) throws -> T) rethrows -> T {
+//  
+//  let tracer = OTelSpans.shared.tracer(scopeName: scopeName)
+//  return try tracer.spanBuilder(spanName: operationName)
+//    .setSpanKind(spanKind: kind)
+//    .withStartedSpan { span in
+//      span.setAttribute(key: .sourceFunction, value: function)
+//      span.setAttribute(key: .sourceFile, value: file)
+//      span.setAttribute(key: .sourceLine, value: String(line))
+//      return try operation(span)
+//    }
+//}
+//
+//public func withSpan<T>(_ operationName: String,
+//                        scopeName: String = "OTelSpans",
+//                        ofKind kind: SpanKind = .internal,
+//                        function: String = #function,
+//                        file: String = #fileID,
+//                        line: UInt = #line,
+//                        _ operation: (any SpanBase) async throws -> T) async rethrows -> T {
+//  
+//  let tracer = OTelSpans.shared.tracer(scopeName: scopeName)
+//  return try await tracer.spanBuilder(spanName: operationName)
+//    .setSpanKind(spanKind: kind)
+//    .withStartedSpan { span in
+//      span.setAttribute(key: .sourceFunction, value: function)
+//      span.setAttribute(key: .sourceFile, value: file)
+//      span.setAttribute(key: .sourceLine, value: String(line))
+//      return try await operation(span)
+//    }
+//}
+//
+//public func withActiveSpan<T>(_ operationName: String,
+//                              scopeName: String = "OTelSpans",
+//                              ofKind kind: SpanKind = .internal,
+//                              function: String = #function,
+//                              file: String = #fileID,
+//                              line: UInt = #line,
+//                              _ operation: (any SpanBase) throws -> T) rethrows -> T {
+//  
+//  let tracer = OTelSpans.shared.tracer(scopeName: scopeName)
+//  return try tracer.spanBuilder(spanName: operationName)
+//    .setSpanKind(spanKind: kind)
+//    .withActiveSpan { span in
+//      span.setAttribute(key: .sourceFunction, value: function)
+//      span.setAttribute(key: .sourceFile, value: file)
+//      span.setAttribute(key: .sourceLine, value: String(line))
+//      return try operation(span)
+//    }
+//}
+//
+//
+//public func withActiveSpan<T>(_ operationName: String,
+//                              scopeName: String = "OTelSpans",
+//                              ofKind kind: SpanKind = .internal,
+//                              function: String = #function,
+//                              file: String = #fileID,
+//                              line: UInt = #line,
+//                              _ operation: (any SpanBase) async throws -> T) async rethrows -> T {
+//  
+//  let tracer = OTelSpans.shared.tracer(scopeName: scopeName)
+//  return try await tracer.spanBuilder(spanName: operationName)
+//    .setSpanKind(spanKind: kind)
+//    .withActiveSpan { span in
+//      span.setAttribute(key: OtelSemanticAttributes.sourceFunction, value: function)
+//      span.setAttribute(key: .sourceFile, value: file)
+//      span.setAttribute(key: .sourceLine, value: String(line))
+//      return try await operation(span)
+//    }
+//}
+//
+//
+//public enum OtelSemanticAttributes: String {
+//  case httpRequestMethod = "http.request.method"
+//  case httpStatusCode = "http.status_code"
+//  
+//  case urlPath = "url.path"
+//  
+//  case sourceFunction = "source.function"
+//  case sourceLine = "source.line"
+//  case sourceFile = "source.file"
+//  
+//  case featureName = "feature.name"
+//}
+//
+//extension SpanBase {
+//  public func setAttribute(key: OtelSemanticAttributes, value: String) {
+//    setAttribute(key: key.rawValue, value: .string(value))
+//  }
+//}
